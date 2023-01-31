cbc78704daf0318faa3567dedaea76722b118636
Owen araujo <31048440+owenaraujo@users.noreply.github.com>
2023-01-31 13:30:37 -0500

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -457,8 +457,8 @@ export default {
       let url = `https://api.whatsapp.com/send?phone=573202446072`;
       window.open(url, "_blank");
     }
-    let tasaBss = ref(255);
-    let tasaDolar = ref(20.89);
+    let tasaBss = ref(224);
+    let tasaDolar = ref(23.03);
     function calculate(valor) {
       if (valor === "pesos") {
         let bolivar = parseFloat(pesos.value / tasaBss.value).toFixed(2);
